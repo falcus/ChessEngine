b5df1b6644074baa2b14ab2ff98aa0401bec52aa
falcus <crdeufel@gmail.com>
2022-12-29 23:22:05 +0200

    first commit

---
+++ ChessEngine.py
@@ -0,0 +1,17 @@
+# Stores Game State. Determines valid moves. Keeps a log of moves.
+
+class GameState():
+    def __init__(self):
+        #Board is an 8*8 2d list, each element represents a piece with colour and class represented by letters.
+        #"--" is an empty space
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR" ]
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp" ]
+            ["--", "--", "--", "--", "--", "--", "--", "--" ]
+            ["--", "--", "--", "--", "--", "--", "--", "--" ]
+            ["--", "--", "--", "--", "--", "--", "--", "--" ]
+            ["--", "--", "--", "--", "--", "--", "--", "--" ]
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp" ]
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR" ]]
+            self.whiteToMove = True
+            self.moveLog = []
---
+++ ChessMain.py
@@ -0,0 +1,4 @@
+# Main driver File. Handles user input and displaying current GameState object.
+
+import pygame as p
+from Chess import ChessEngine
